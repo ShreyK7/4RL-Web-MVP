ac0c6132f7ef1141083f4b7059af5ee9046ecd6e
SHREY KHATER <sk10189@stern.nyu.edu>
2025-11-12 12:11:29 -0500

    initial project setup

---
+++ src/components/auth/login.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function Login() {
+  return (
+    <div>
+      <h1>Login to 4RL</h1>
+    </div>
+  );
+}
---
+++ src/components/auth/signup.tsx
@@ -0,0 +1,7 @@
+export default function Signup() {
+  return (
+    <div>
+      <h1>Signup for 4RL</h1>
+    </div>
+  );
+}
